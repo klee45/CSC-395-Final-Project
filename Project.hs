6a05983fe3589e43cd81572597c33b998614e8ac
Zergonomics <kjjhawk@gmail.com>
2016-12-07 02:57:50 -0600

    Added glowing animation and fixed a few more bugs. Everything seems to be working fine right now

--- Project.hs
+++ Project.hs
@@ -14,10 +14,7 @@ import qualified Data.Matrix as M
 
 {-- TODO
 
-Finish conversion from our images to pngs through juicypixels
-
-Remove / rename pixel to remove x y location and instead fill entire rect
-    or edit conversion to fill remaining elements with transparent
+Exporting
 
 -}
 
@@ -47,7 +44,11 @@ fromPng path = do
                 return (makeImage result)
 
 
+                
+{---------------------------------------------------------------------------------}
 
+exColorTransparent = (MyColor 0 0 0 0)
+exColorWhite       = (MyColor 256 256 256 1)
 
                 
 {--------------------------- Loading some examples -------------------------------}
@@ -61,20 +62,37 @@ exF2 = imageMap (\c -> colorAdd c (-20) 20 0)
 exF3 = imageInvert
 
 -- Expand image 5 pixels on all sides with transparent pixels
-exF4 = imageExpandBorders (MyColor 0 0 0 0) 5 5 5 5
+exF4 = imageExpandBorders exColorTransparent 5 5 5 5
+exF5 = imageGetOutline 2
 
-exA1 ani = (animationBlink ani 9)
+exA1 = animationBlink (MyColor 256 256 256 1) 300
 
-example1    = do 
-                img <- exampleRaw1
-                return $ toDrawable (exF4 (exF3 (exF2 (exF1 img))))
-                                    5 5 0.25 0.30
-                                    
-example2    = do
-                img <- exampleRaw2
-                return $ mkAnimation (animationBlink img 17)
-                                     25 0 0.5 0.2
-                                    
+exA2 = animationGlow [(MyColor 255 255 50  1),
+                      (MyColor 255 150 25   1)]
+                     [(MyColor 255 150 25   1),
+                      (MyColor 255 0   0   1)]
+                     17
+
+example1 = do 
+            img <- exampleRaw1
+            return $ toDrawable (exF5 (exF4 (exF3 (exF2 (exF1 img)))))
+                                5 5 0.25 0.30
+                                     
+example2 = do
+            img <- exampleRaw2
+            return $ mkAnimation (exA1 (exF4 img))
+                                 25 5 0.6 0.4
+                      
+example3 = do
+            img <- exampleRaw2
+            return $ mkAnimation (exA2 (exF4 img))
+                                 25 5 0.6 0.4
+                                     
+example4 = do
+            img <- exampleRaw2
+            let (MyImage matrix) = img
+            return $ toDrawable (imageOverlay (blankImage (M.ncols matrix) (M.nrows matrix)) img)
+                                10 10 0.3 0.3        
 background = do img <- backgroundRaw 
                 return $ toDrawable img 0 0 1.0 1.0
 
@@ -113,11 +131,17 @@ display iter = do
   a <- get iter
   i1 <- background
   i2 <- example1
+  i3 <- example4
+
   a1 <- example2
+  a2 <- example3
+  
   --drawCanvas
   --draw i1
   draw i2
+  --draw i3
   drawAnimation a1 a
+  drawAnimation a2 a
   flush                       -- Sends openGL commands to graphics for display
 
 draw :: Drawable -> IO ()
@@ -159,7 +183,13 @@ blueRotate (MyImage pixels w h) v = MyImage (map (\((MyColor r g b a), point) ->
 {-----------------------------   Library Functions   -------------------------}
 
 blankImage :: Int -> Int -> MyImage
-blankImage w h = MyImage $ M.fromList h w (replicate (w * h) (MyColor 256 256 256 1))
+blankImage w h = emptyImage w h $ exColorWhite
+
+transparentImage :: Int -> Int -> MyImage
+transparentImage w h = emptyImage w h $ exColorTransparent
+
+emptyImage :: Int -> Int -> MyColor -> MyImage
+emptyImage w h color = MyImage $ M.fromList h w (replicate (w * h) color)
 
 {-
 gradientImage :: MyImage -> MyImage
@@ -193,6 +223,15 @@ instance Show MyColor where
                              ", " ++ show a ++ ")"
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
 {----------------------------    Smart Constructors    -----------------------------}
         
 mkColor :: Int -> Int -> Int -> Double -> MyColor
@@ -212,7 +251,15 @@ mkColorBounded r g b a = mkColor (bound r) (bound g) (bound b) (bounda a)
             | a < 0.0   = 0.0
             | a > 1.0   = 1.0
             | otherwise = a
-                         
+                        
+
+
+
+
+
+
+
+                        
 {----------------------------    Images and Matrices     ---------------------------}
 
 -- MyImage contains a list of MyPixels
@@ -249,14 +296,15 @@ imageExpandBorders replace left right up down (MyImage old) = MyImage new
                         (down + rows)
                         (right + cols)
                         old
-        s2 = rev (tra (rev s1))           -- Reverse rows, transpose, reverse cols
+        s2 = flip s1                      -- Reverse rows, transpose, reverse cols
         s3 = M.extendTo replace
                         (up + rows + down)
                         (left + cols + right)
-                        (M.fromLists s2)
-        new = M.fromLists $ rev (tra (rev s3))
+                        s2
+        new = flip s3 
         rev = map reverse . M.toLists
         tra = M.transpose . M.fromLists
+        flip = tra . rev . tra . rev
 
         
         
@@ -324,7 +372,6 @@ toDrawable (MyImage matrix) x y w h = Drawable (M.toList (matrixMapXY helper mat
 {-------------------------------------------- Image Functions ------------------------------------------}
 
 {-
-
 -- Split horizontally
 -- If n doesn't make sense or the image can't be split,
 -- returns the same image
@@ -346,12 +393,12 @@ splitV image@(MyImage pairs w h) n
     | otherwise        = undefined
     where
         temp = [[0..(h `div` n)]]
-    
--}      
+-}
 
 imageReplaceColor :: Int -> MyColor -> MyColor -> MyImage -> MyImage
 imageReplaceColor m compare@(MyColor r' g' b' _) new image = imageMap helper image
     where
+        helper old@(MyColor _ _ _ 0)        = old
         helper old@(MyColor r g b a)
             | withinR && withinG && withinB = new
             | otherwise                     = old 
@@ -359,6 +406,12 @@ imageReplaceColor m compare@(MyColor r' g' b' _) new image = imageMap helper ima
                 withinR = (abs (r' - r)) <= m
                 withinG = (abs (g' - g)) <= m
                 withinB = (abs (b' - b)) <= m
+                
+imageReplaceAllColor :: MyColor -> MyImage -> MyImage
+imageReplaceAllColor color = imageMap helper
+    where
+        helper old@(MyColor _ _ _ 0) = old
+        helper _                     = color
 
 imageInvert :: MyImage -> MyImage
 imageInvert image = imageMap (\(MyColor r g b a) -> mkColorBounded (255 - r)
@@ -374,11 +427,61 @@ imageGrayscale image = imageMap helper image
             where
                 l = round ((times 0.2126 r) + (times 0.7152 g) + (times 0.0722 b))
                 times dbl v = dbl * (fromIntegral (v :: Int) :: Double)
+                
+{-
+    Extremely simple edge detection algorithm
+    Simply checks if a pixel both
+        1. is transparent (alpha 0)
+        2. has a nontransparent neighbor
+           within the given range
+           
+    Returns a white pixelmap
+-}
+imageGetOutline :: Int -> MyImage -> MyImage
+imageGetOutline range (MyImage matrix) = MyImage (matrixMapXY checkRange matrix)
+    where
+        cols = M.ncols matrix
+        rows = M.nrows matrix
+        checkRange (MyColor _ _ _ 0) x y
+            | foldl (||) False (getResults x y)  = exColorWhite
+            | otherwise                          = exColorTransparent
+        checkRange _ _ _                         = exColorTransparent
+        getResults x y = map checkPixel [(x',y') | x' <- [(x - range)..(x + range)],
+                                                   y' <- [(y - range)..(y + range)]]
+            where
+                checkPixel (x',y')
+                    | (x' < 1)    ||
+                      (x' > cols) ||
+                      (y' < 1)    ||
+                      (y' > rows) = False
+                    | otherwise                 = notTransparent (M.getElem y' x' matrix)
+                notTransparent (MyColor _ _ _ 0) = False
+                notTransparent _                 = True
 
+-- Assumes the images are the same size
+imageOverlay :: MyImage -> MyImage -> MyImage
+imageOverlay (MyImage bottom) (MyImage top) =
+    MyImage $ M.fromList (M.nrows bottom) (M.ncols top) (map helper (zip (M.toList bottom) (M.toList top)))
+    where
+        helper (c,(MyColor _ _ _ 0)) = c
+        helper (c,c')                = c'
+                
 {--------------------------------------- Individual Color Functions --------------------------------------}
 
 colorAdd :: MyColor -> Int -> Int -> Int -> MyColor
 colorAdd (MyColor r g b a) rp gp bp = mkColorBounded (r + rp) (g + gp) (b + bp) a
+
+-- Preserves opacity
+colorBlend :: MyColor -> MyColor -> Double -> MyColor
+colorBlend (MyColor r1 g1 b1 a) (MyColor r2 g2 b2 _) ratio =
+    (MyColor (change r1 r2)
+             (change g1 g2)
+             (change b1 b2)
+             a)
+    where
+        change v1 v2 = floor ((toDouble v1) + (toDouble (v2 - v1)) * ratio)
+        toDouble v = (fromIntegral (v :: Int) :: Double)
+
 
 {--------------------------------------- Animation Functions ---------------------------------------------}
 {-
@@ -392,31 +495,59 @@ colorAdd (MyColor r g b a) rp gp bp = mkColorBounded (r + rp) (g + gp) (b + bp) 
 -- white and return
 --
 -- Additionally, frames must be >= 3
-animationBlink :: MyImage -> Int -> [MyImage]
-animationBlink image frames = map helper ([0..frames] ++ [frames-1,frames-2..0])
+animationBlink :: MyColor -> Int -> MyImage -> [MyImage]
+animationBlink c' frames image = map helper ([0..frames] ++ [frames-1,frames-2..0])
     where
-        helper frame = imageMap (\c -> colorToWhite c frame) image
-        colorToWhite (MyColor r g b a) frame = (MyColor (whiten r ratio) (whiten g ratio) (whiten b ratio) a)
+        helper frame = imageMap (\c -> colorBlend c c' ratio) image
             where
                 ratio = (toDouble frame) / (toDouble frames)
-        whiten v ratio = floor ((toDouble v) + (toDouble (255 - v)) * ratio)
-        toDouble v = (fromIntegral (v :: Int) :: Double)
+                toDouble v = (fromIntegral (v :: Int) :: Double)
+
+-- Both color lists must be the same length
+animationGlow :: [MyColor] -> [MyColor] -> Int -> MyImage -> [MyImage]
+animationGlow colors colors' frames image@(MyImage matrix) = map helper ([0..frames] ++ [frames-1,frames-2..0])
+    where
+        rows = M.nrows matrix
+        cols = M.ncols matrix
+        -- Pairs up outlines with their corresponding initial colors
+        outlinePairs = zip (map (\v -> imageGetOutline v image)
+                                [1..(length colors)])
+                           colors
+        -- Uses outline-color pairs to color outlines
+        coloredOutlines = map (\(image, color) -> imageReplaceAllColor color image)
+                              outlinePairs
+        -- Recolors based on the given frame ratio and the given ending colors
+        frameOutlines ratio = map (\(image, color') -> imageMap (\c -> colorBlend c color' ratio)
+                                                                image)
+                                  (zip coloredOutlines colors')
+        -- Places outlines on top of one another to create full outline
+        helper frame = foldl imageOverlay
+                             (transparentImage cols rows)
+                             (reverse (frameOutlines ratio))
+            where
+                ratio = (toDouble frame) / (toDouble frames)
+                toDouble v = (fromIntegral (v :: Int) :: Double)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
 
 
 
